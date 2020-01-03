eae6ea466185babd0dbbd88b67a44bc4536c8ec1
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-01-03 12:55:22 +0100

    Added design page.

--- src/main/java/sia/tacocloud/web/DesignTacoController.java
+++ src/main/java/sia/tacocloud/web/DesignTacoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacocloud.Ingredient;
 import sia.tacocloud.Ingredient.Type;
+import sia.tacocloud.Taco;
 
 import java.util.Arrays;
 import java.util.List;
