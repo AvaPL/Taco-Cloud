00a0b400183630d81cff5304e44a28fc83b4b9a0
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-02-10 18:03:32 +0100

    Added REST controller for getting tacos as JSON.

---
+++ src/main/java/sia/tacocloud/web/api/DesignTacoRestController.java
@@ -0,0 +1,41 @@
+package sia.tacocloud.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sia.tacocloud.Taco;
+import sia.tacocloud.data.TacoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoRestController {
+
+    private final TacoRepository tacoRepository;
+
+    @Autowired
+    public DesignTacoRestController(TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest pageRequest = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(pageRequest).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") long id) {
+        Optional<Taco> tacoOptional = tacoRepository.findById(id);
+        if (tacoOptional.isPresent())
+            return new ResponseEntity<>(tacoOptional.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
