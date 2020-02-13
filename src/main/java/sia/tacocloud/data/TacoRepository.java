2c1f8f237632299f70b3491b9d66e33edf05e396
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-02-13 13:24:30 +0100

    Added more readable /tacos/search/recent endpoint.

--- src/main/java/sia/tacocloud/data/TacoRepository.java
+++ src/main/java/sia/tacocloud/data/TacoRepository.java
@@ -1,7 +1,14 @@
 package sia.tacocloud.data;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import sia.tacocloud.Taco;
 
+@RepositoryRestResource(collectionResourceRel = "tacos", path = "tacos")
 public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
+
+    @RestResource(path = "recent", rel = "recent")
+    public Iterable<Taco> findTop12ByOrderByCreatedAtDesc();
+
 }
